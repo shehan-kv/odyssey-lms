7ae3c5376c3a4b5304c511e1c91625ccb7b7f405
shehan-kv <kvsm066@gmail.com>
2024-05-11 11:16:38 +0530

    refactor(db): add on delete constraints for sqlite

--- internal/database/schema/sqlite/migrations/003_user_roles.sql
+++ internal/database/schema/sqlite/migrations/003_user_roles.sql
@@ -2,8 +2,8 @@
 CREATE TABLE IF NOT EXISTS user_roles (
   user_id INTEGER NOT NULL,
   role_id INTEGER NOT NULL,
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (role_id) REFERENCES roles(id)
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE
 );
 
 

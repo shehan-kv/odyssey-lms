b9993ffd33e35c1c65e63ee285b0a6ff966e96dd
shehan-kv <kvsm066@gmail.com>
2024-05-18 06:13:27 +0530

    feat(db): add default roles on up migrations

--- internal/db/schema/mysql/migrations/002_roles.sql
+++ internal/db/schema/mysql/migrations/002_roles.sql
@@ -1,9 +1,13 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS roles (
   id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  name VARCHAR(50) NOT NULL UNIQUE
+  name VARCHAR(50) NOT NULL UNIQUE,
+  is_default BOOLEAN NOT NULL
 );
 
+INSERT INTO roles (name, is_default) VALUES ('administrator', true);
+INSERT INTO roles (name, is_default) VALUES ('instructor', true);
+INSERT INTO roles (name, is_default) VALUES ('student', true);
 
 -- +goose Down
 DROP TABLE IF EXISTS roles;

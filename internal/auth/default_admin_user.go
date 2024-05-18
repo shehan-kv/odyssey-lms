ab692cd1eb4b1dfa96a29ff5e326d3d612e8faa8
shehan-kv <kvsm066@gmail.com>
2024-05-18 06:30:26 +0530

    feat(server): create admin user on startup

---
+++ internal/auth/default_admin_user.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"math/rand"
+
+	"odyssey.lms/internal/colors"
+	"odyssey.lms/internal/db"
+	"odyssey.lms/internal/db/params"
+)
+
+func CreateDefaultAdminUser() {
+
+	ctx := context.Background()
+	dbQuery := db.GetDBQuery()
+	adminCount, _ := dbQuery.CountUsersByRole(ctx, "administrator")
+
+	if adminCount != 0 {
+		return
+	}
+
+	tempPassword := getRandomString(10)
+	hashedPassword, err := HashPassword(tempPassword)
+	if err != nil {
+		log.Fatal(colors.RedBold + "[ ERROR ] Failed to create admin account" + colors.Reset)
+	}
+
+	adminUser, err := dbQuery.CreateUser(ctx, params.CreateUser{
+		FirstName: "Default",
+		LastName:  "Administrator",
+		Email:     "admin@lms.local",
+		Password:  hashedPassword,
+		Bio: sql.NullString{
+			String: "This is the default admin account of the system, created when other admin " +
+				"accounts couldn't be found. Please delete this account once you create an admin account",
+			Valid: true,
+		},
+	})
+	if err != nil {
+		log.Fatal(colors.RedBold + "[ ERROR ] Failed to create admin account" + colors.Reset)
+	}
+
+	_, err = dbQuery.AssignUserRole(ctx, params.AssignUserRole{UserID: adminUser.ID, RoleName: "administrator"})
+	if err != nil {
+		_ = dbQuery.DeleteUserById(ctx, adminUser.ID)
+		log.Fatal(colors.RedBold + "[ ERROR ] Failed to create admin account" + colors.Reset)
+	}
+
+	log.Println(colors.Blue + "[ INFO ] Created " + colors.Reset + colors.Yellow + "admin@lms.local " + colors.Reset +
+		colors.Blue + "account with password: " + colors.Reset + colors.Yellow + tempPassword + colors.Reset)
+
+}
+
+func getRandomString(length int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	randomLetters := make([]byte, length)
+	for i := range randomLetters {
+		randomLetters[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(randomLetters)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"odyssey.lms/internal/auth"
+	"odyssey.lms/internal/colors"
+	"odyssey.lms/internal/db"
 )
+
+func init() {
+	db.CheckDBSettings()
+	db.RunMigrations()
+	auth.CreateDefaultAdminUser()
+}
 
 func RunApplication() {
 
@@ -12,7 +23,14 @@ func RunApplication() {
 		fmt.Fprint(w, "<h1>Hello From Odyssey LMS ! </h1>")
 	})
 
-	fmt.Print("Starting HTTP Server...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	listenOn := os.Getenv("LISTEN_ON")
+	if listenOn == "" {
+		listenOn = ":8080"
+	}
 
+	log.Println(colors.GreenBold + "[ INFO ] Server listening on " + listenOn + colors.Reset)
+	err := http.ListenAndServe(listenOn, nil)
+	if err != nil {
+		log.Fatal(colors.RedBold + "[ ERROR ] Failed to listen on " + listenOn + colors.Reset)
+	}
 }

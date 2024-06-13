b0ea3ab9ace067572ed5f6d685d59cb8f7b91356
shehan-kv <kvsm066@gmail.com>
2024-06-13 21:21:45 +0530

    feat(web): add a card to show recent events

---
+++ web/ui/src/lib/components/ui/table/table-cell.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { cn } from '$lib/utils.js';
+	let className = undefined;
+	export { className as class };
+</script>
+
+<td
+	class={cn('px-4 py-3 align-middle [&:has([role=checkbox])]:pr-0', className)}
+	{...$$restProps}
+	on:click
+	on:keydown
+>
+	<slot />
+</td>

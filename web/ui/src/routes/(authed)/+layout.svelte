f7e1d05bae2c9ed60957a7e5a32577c54a615daf
shehan-kv <kvsm066@gmail.com>
2024-06-13 21:32:33 +0530

    feat(web): update authed index page to show admin dashboard

--- web/ui/src/routes/(authed)/+layout.svelte
+++ web/ui/src/routes/(authed)/+layout.svelte
@@ -1,7 +1,17 @@
 <script>
 	import { goto } from '$app/navigation';
-	import FullScreenLoader from '$lib/components/ui/fullScreenLoader.svelte';
+	import FullScreenLoader from '$lib/components/fullScreenLoader.svelte';
+	import Header from '$lib/components/header.svelte';
+	import MobileSidebar from '$lib/components/mobileSidebar.svelte';
+	import ScreenSizeWidget from '$lib/components/screenSizeWidget.svelte';
+	import Sidebar from '$lib/components/sidebar.svelte';
 	import { onMount } from 'svelte';
+
+	let mobileMenu = false;
+
+	const handleMobileMenu = () => {
+		mobileMenu = !mobileMenu;
+	};
 
 	let loading = true;
 
@@ -20,8 +30,19 @@
 	});
 </script>
 
-<slot />
-
 {#if loading}
 	<FullScreenLoader />
 {/if}
+
+{#if mobileMenu}
+	<MobileSidebar on:mobileMenuToggle={handleMobileMenu} />
+{/if}
+
+<div class="flex flex-col gap-y-2 h-screen bg-neutral-100 dark:bg-neutral-900 p-2">
+	<Header on:mobileMenuToggle={handleMobileMenu} />
+	<div class="flex grow gap-2 overflow-hidden">
+		<Sidebar />
+
+		<slot></slot>
+	</div>
+</div>
--- web/ui/src/routes/(authed)/+page.svelte
+++ web/ui/src/routes/(authed)/+page.svelte
@@ -1,1 +1,24 @@
-<h1>Odyssey LMS</h1>
+<script>
+	import UserRegistrationsCard from '$lib/components/userRegistrationsCard.svelte';
+	import ServerInformationCard from '$lib/components/serverInformationCard.svelte';
+	import RecentEventsCard from '$lib/components/recentEventsCard.svelte';
+	import RecentCoursesCard from '$lib/components/recentCoursesCard.svelte';
+	import SupportTicketsCard from '$lib/components/supportTicketsCard.svelte';
+</script>
+
+<svelte:head>
+	<title>System Console</title>
+</svelte:head>
+
+<div class="grow space-y-2 overflow-scroll">
+	<div class="grid grid-cols-1 lg:grid-cols-2 gap-2 w-full">
+		<UserRegistrationsCard />
+		<div class="flex flex-col gap-2">
+			<ServerInformationCard />
+			<RecentEventsCard />
+		</div>
+	</div>
+
+	<RecentCoursesCard />
+	<SupportTicketsCard />
+</div>

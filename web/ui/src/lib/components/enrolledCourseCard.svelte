f17ee4d597c1d2294a80dc03ff0cc3cccd9043cc
shehan-kv <kvsm066@gmail.com>
2024-06-15 12:38:47 +0530

    feat(web): add page to show enrolled courses

---
+++ web/ui/src/lib/components/enrolledCourseCard.svelte
@@ -0,0 +1,47 @@
+<script>
+	import ImagePlaceholder from './imagePlaceholder.svelte';
+	import LecternIcon from './lecternIcon.svelte';
+
+	/**
+	 * @type {{
+	 * 	img: string,
+	 * 	name: string,
+	 * 	code: string,
+	 * 	description:string,
+	 * 	instructor : string[],
+	 * 	completed: number
+	 * }}
+	 */
+	export let course;
+</script>
+
+<div class="max-w-md">
+	<div class="space-y-2">
+		<div class="h-60 rounded overflow-hidden">
+			{#if course && course.img}
+				<img src={course.img} alt={course.name} />
+			{:else}
+				<ImagePlaceholder />
+			{/if}
+		</div>
+		<p class="font-semibold">{course.name}</p>
+		<p class="text-neutral-500">{course.code}</p>
+		<p class="text-sm text-ellipsis overflow-hidden h-20">
+			{course.description}
+		</p>
+		<p>
+			<span class="flex items-center gap-2 text-sm"
+				><LecternIcon size={16} />{course.instructor.join(', ')}</span
+			>
+		</p>
+	</div>
+	<div class="flex items-center gap-8 mt-4">
+		<div class="h-1.5 rounded w-full bg-neutral-200 dark:bg-neutral-900 overflow-hidden">
+			<div
+				class="h-1.5 rounded bg-neutral-400 dark:bg-neutral-600"
+				style={`width: ${course.completed}%;`}
+			></div>
+		</div>
+		<p class="text-xs">{course.completed}%</p>
+	</div>
+</div>
---
+++ web/ui/src/routes/(authed)/enrolled-courses/+page.svelte
@@ -0,0 +1,83 @@
+<script>
+	import CardErrorRetry from '$lib/components/cardErrorRetry.svelte';
+	import CardLoader from '$lib/components/cardLoader.svelte';
+	import EnrolledCourseCard from '$lib/components/enrolledCourseCard.svelte';
+	import { GraduationCap } from 'lucide-svelte';
+	import { onMount } from 'svelte';
+
+	let loading = true;
+	let fetchError = false;
+
+	/**
+	 * @type {{
+	 * 	img: string,
+	 * 	name: string,
+	 * 	code: string,
+	 * 	description:string,
+	 * 	instructor : string[],
+	 * 	completed: number
+	 * }[]}
+	 */
+	let data;
+
+	const fetchData = () => {
+		loading = true;
+		fetchError = false;
+
+		fetch('/api/course/enrolled')
+			.then((response) => {
+				if (response.status != 200) {
+					fetchError = true;
+					loading = false;
+				} else {
+					return response.json();
+				}
+			})
+			.then((parsedData) => {
+				data = parsedData;
+				loading = false;
+			})
+			.catch(() => {
+				fetchError = true;
+				loading = false;
+			});
+	};
+
+	onMount(() => {
+		fetchData();
+	});
+</script>
+
+<svelte:head>
+	<title>Enrolled Courses</title>
+</svelte:head>
+
+<div
+	class="relative flex flex-col py-6 px-4 rounded bg-white dark:bg-neutral-950 overflow-scroll w-full"
+>
+	{#if loading}
+		<CardLoader />
+	{/if}
+	{#if fetchError}
+		<CardErrorRetry on:retry={fetchData} />
+	{/if}
+
+	<div class="flex gap-2 text-sm font-semibold">
+		<GraduationCap size={20} />
+		<p>Enrolled Courses</p>
+	</div>
+
+	{#if !fetchError && (!data || data.length == 0)}
+		<div class="flex grow justify-center items-center">
+			<p class="text-sm text-neutral-400 dark:text-neutral-600">No Enrolled Courses Found</p>
+		</div>
+	{/if}
+
+	{#if data && data.length > 0}
+		<div class="flex flex-wrap gap-x-10 gap-y-16 mt-10">
+			{#each data as course}
+				<EnrolledCourseCard {course} />
+			{/each}
+		</div>
+	{/if}
+</div>

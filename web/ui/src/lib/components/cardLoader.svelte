9c01e8e55f4ca552fdc5db3df070a09f0d14b4e4
shehan-kv <kvsm066@gmail.com>
2024-06-16 22:25:03 +0530

    feat(web): add higher z index to card loader

--- web/ui/src/lib/components/cardLoader.svelte
+++ web/ui/src/lib/components/cardLoader.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-	class="absolute inset-0 flex items-center justify-center bg-white dark:bg-neutral-950 top-0 left-0"
+	class="absolute z-20 inset-0 flex items-center justify-center bg-white dark:bg-neutral-950 top-0 left-0"
 >
 	<Loader class="stroke-neutral-950 dark:stroke-neutral-100 w-8" />
 </div>

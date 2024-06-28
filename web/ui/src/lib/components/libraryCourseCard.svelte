3160a7ca38b659fe39abfa75024b6f05af75bdeb
shehan-kv <kvsm066@gmail.com>
2024-06-28 16:12:07 +0530

    refactor(web): link to course page from library page

--- web/ui/src/lib/components/libraryCourseCard.svelte
+++ web/ui/src/lib/components/libraryCourseCard.svelte
@@ -1,31 +1,43 @@
 <script>
-	import { Users2 } from 'lucide-svelte';
 	import ImagePlaceholder from './imagePlaceholder.svelte';
 
 	/**
 	 * @type {{
+	 * 	id: number,
 	 * 	image: string,
 	 * 	name: string,
 	 * 	code: string,
 	 * 	description:string,
 	 *  category: string,
-	 * 	enrolled: number
 	 * }}
 	 */
 	export let course;
 </script>
 
-<div class="max-w-md space-y-2">
-	<div class="h-60 rounded overflow-hidden">
-		{#if course && course.image}
-			<img src={`/uploads/${course.image}`} alt={course.name} class="object-cover" />
-		{:else}
-			<ImagePlaceholder />
-		{/if}
+<div class="relative max-w-md">
+	<div class="space-y-2">
+		<div class="h-60 rounded overflow-hidden">
+			{#if course && course.image}
+				<img
+					src={`/uploads/${course.image}`}
+					alt={course.name}
+					class="w-full h-full object-cover"
+				/>
+			{:else}
+				<ImagePlaceholder />
+			{/if}
+		</div>
+		<div class="space-y-1">
+			<a
+				href={`/course-library/${course.id}`}
+				class="inline-block font-semibold after:content-[' '] after:absolute after:top-0 after:left-0 after:inset-0"
+			>
+				{course.name}
+			</a>
+			<p class="text-neutral-500 text-sm">{course.code} | <span>{course.category}</span></p>
+		</div>
+		<p class="text-sm line-clamp-4">
+			{course.description}
+		</p>
 	</div>
-	<p class="font-semibold">{course.name}</p>
-	<p class="text-neutral-500 text-sm">{course.code} | <span>{course.category}</span></p>
-	<p class="text-sm text-ellipsis overflow-hidden h-20">
-		{course.description}
-	</p>
 </div>

1061cece5042c8535b15e3513abbfd4cdefc4cd2
shehan-kv <kvsm066@gmail.com>
2024-06-29 21:11:19 +0530

    feat(web): update sidebar links based on user role

--- web/ui/src/lib/components/mobileSidebar.svelte
+++ web/ui/src/lib/components/mobileSidebar.svelte
@@ -13,6 +13,7 @@
 	import SidebarLink from './sidebarLink.svelte';
 	import Button from './ui/button/button.svelte';
 	import { createEventDispatcher } from 'svelte';
+	import { userRole } from '../../stores/store';
 
 	let dispatch = createEventDispatcher();
 </script>
@@ -37,13 +38,20 @@
 			</Button>
 		</div>
 		<div class="flex flex-col">
-			<SidebarLink to="/"><Activity size={20} /> System Console</SidebarLink>
-			<SidebarLink to="/create-course"><Plus size={20} />Create Course</SidebarLink>
+			{#if $userRole == 'administrator'}
+				<SidebarLink to="/"><Activity size={20} /> System Console</SidebarLink>
+				<SidebarLink to="/create-course"><Plus size={20} />Create Course</SidebarLink>
+			{/if}
 			<SidebarLink to="/enrolled-courses"><GraduationCap size={20} />Enrolled Courses</SidebarLink>
 			<SidebarLink to="/course-library"><LibrarySquare size={20} />Course Library</SidebarLink>
-			<SidebarLink to="/iam"><Users size={20} /> IAM</SidebarLink>
-			<SidebarLink to="/events"><Info size={20} /> Events</SidebarLink>
-			<SidebarLink to="/support-tickets"><Ticket size={20} />Support Tickets</SidebarLink>
+			{#if $userRole == 'administrator'}
+				<SidebarLink to="/iam"><Users size={20} /> IAM</SidebarLink>
+				<SidebarLink to="/events"><Info size={20} /> Events</SidebarLink>
+				<SidebarLink to="/support-tickets"><Ticket size={20} />Support Tickets</SidebarLink>
+			{/if}
+			{#if $userRole == 'student'}
+				<SidebarLink to="/my-support-tickets"><Ticket size={20} />My Support Tickets</SidebarLink>
+			{/if}
 			<SidebarLink to="/settings"><Settings size={20} />Settings</SidebarLink>
 		</div>
 	</div>
--- web/ui/src/lib/components/sidebar.svelte
+++ web/ui/src/lib/components/sidebar.svelte
@@ -10,17 +10,25 @@
 		Users
 	} from 'lucide-svelte';
 	import SidebarLink from './sidebarLink.svelte';
+	import { userRole } from '../../stores/store';
 </script>
 
 <div class="hidden xl:block bg-white dark:bg-neutral-950 rounded py-4 w-60">
 	<div class="flex flex-col">
-		<SidebarLink to="/"><Activity size={20} /> System Console</SidebarLink>
-		<SidebarLink to="/create-course"><Plus size={20} />Create Course</SidebarLink>
+		{#if $userRole == 'administrator'}
+			<SidebarLink to="/"><Activity size={20} /> System Console</SidebarLink>
+			<SidebarLink to="/create-course"><Plus size={20} />Create Course</SidebarLink>
+		{/if}
 		<SidebarLink to="/enrolled-courses"><GraduationCap size={20} />Enrolled Courses</SidebarLink>
 		<SidebarLink to="/course-library"><LibrarySquare size={20} />Course Library</SidebarLink>
-		<SidebarLink to="/iam"><Users size={20} /> IAM</SidebarLink>
-		<SidebarLink to="/events"><Info size={20} /> Events</SidebarLink>
-		<SidebarLink to="/support-tickets"><Ticket size={20} />Support Tickets</SidebarLink>
+		{#if $userRole == 'administrator'}
+			<SidebarLink to="/iam"><Users size={20} /> IAM</SidebarLink>
+			<SidebarLink to="/events"><Info size={20} /> Events</SidebarLink>
+			<SidebarLink to="/support-tickets"><Ticket size={20} />Support Tickets</SidebarLink>
+		{/if}
+		{#if $userRole == 'student'}
+			<SidebarLink to="/my-support-tickets"><Ticket size={20} />My Support Tickets</SidebarLink>
+		{/if}
 		<SidebarLink to="/settings"><Settings size={20} />Settings</SidebarLink>
 	</div>
 </div>

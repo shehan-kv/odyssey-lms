305522e410fd3fe0e6641097e8b35d0c5c247b92
shehan-kv <kvsm066@gmail.com>
2024-07-01 10:15:46 +0530

    fix(web): close mobile menu on click

--- web/ui/src/lib/components/mobileSidebar.svelte
+++ web/ui/src/lib/components/mobileSidebar.svelte
@@ -39,20 +39,38 @@
 		</div>
 		<div class="flex flex-col">
 			{#if $userRole == 'administrator'}
-				<SidebarLink to="/"><Activity size={20} /> System Console</SidebarLink>
-				<SidebarLink to="/create-course"><Plus size={20} />Create Course</SidebarLink>
+				<SidebarLink to="/" on:click={() => dispatch('mobileMenuToggle')}>
+					<Activity size={20} /> System Console
+				</SidebarLink>
+				<SidebarLink to="/create-course" on:click={() => dispatch('mobileMenuToggle')}>
+					<Plus size={20} />Create Course
+				</SidebarLink>
 			{/if}
-			<SidebarLink to="/enrolled-courses"><GraduationCap size={20} />Enrolled Courses</SidebarLink>
-			<SidebarLink to="/course-library"><LibrarySquare size={20} />Course Library</SidebarLink>
+			<SidebarLink to="/enrolled-courses" on:click={() => dispatch('mobileMenuToggle')}>
+				<GraduationCap size={20} />Enrolled Courses
+			</SidebarLink>
+			<SidebarLink to="/course-library" on:click={() => dispatch('mobileMenuToggle')}>
+				<LibrarySquare size={20} />Course Library
+			</SidebarLink>
 			{#if $userRole == 'administrator'}
-				<SidebarLink to="/iam"><Users size={20} /> IAM</SidebarLink>
-				<SidebarLink to="/events"><Info size={20} /> Events</SidebarLink>
-				<SidebarLink to="/support-tickets"><Ticket size={20} />Support Tickets</SidebarLink>
+				<SidebarLink to="/iam" on:click={() => dispatch('mobileMenuToggle')}>
+					<Users size={20} /> IAM
+				</SidebarLink>
+				<SidebarLink to="/events" on:click={() => dispatch('mobileMenuToggle')}>
+					<Info size={20} /> Events
+				</SidebarLink>
+				<SidebarLink to="/support-tickets" on:click={() => dispatch('mobileMenuToggle')}>
+					<Ticket size={20} />Support Tickets
+				</SidebarLink>
 			{/if}
 			{#if $userRole == 'student'}
-				<SidebarLink to="/my-support-tickets"><Ticket size={20} />My Support Tickets</SidebarLink>
+				<SidebarLink to="/my-support-tickets" on:click={() => dispatch('mobileMenuToggle')}>
+					<Ticket size={20} />My Support Tickets
+				</SidebarLink>
 			{/if}
-			<SidebarLink to="/settings"><Settings size={20} />Settings</SidebarLink>
+			<SidebarLink to="/settings" on:click={() => dispatch('mobileMenuToggle')}>
+				<Settings size={20} />Settings
+			</SidebarLink>
 		</div>
 	</div>
 </div>
